cc92a72139bb226b81ea8a8f22e9362b098eb3f0
unknown <vianove13@yandex.ru>
2020-09-25 22:19:15 +0300

    Update

--- updater.cpp
+++ updater.cpp
@@ -1,126 +1,120 @@
-#include <fstream>
-#include <iostream>
 #include <vector>
 #include <windows.h>
+#include <filesystem>
 
 class Updater {
 private:
-	const char* path;
-	std::ifstream file;
-	size_t fileSize;
-	uint32_t codeOffset;
+	BYTE* fileBytes = nullptr;
 
-	uint32_t world_offset;
-	uint32_t objects_offset;
-	uint32_t names_offset;
+	size_t fileSize = 0;
+	uint32_t codeOffset = 0;
+
+	uint32_t world_offset = 0;
+	uint32_t objects_offset = 0;
+	uint32_t names_offset = 0;
+	uint32_t pool_offset = 0;
 private:
-	unsigned long GetInt(char* hex) {
-		const auto uhex = reinterpret_cast<unsigned char*>(hex);
-		unsigned long num = uhex[0] << 0 | uhex[1] << 8 | uhex[2] << 16 | uhex[3] << 24;
-		return num;
-	}
-	bool CompareByteArray(unsigned char* data, unsigned char* sig, size_t size) {
+	bool CompareByteArray(BYTE* data, BYTE* sig, size_t size) {
 		for (size_t i = 0; i < size; i++) {
 			if (sig[i] == 0x00) continue;
 			if (data[i] != sig[i]) return false;
 		}
 		return true;
 	}
-	size_t FindSignature(unsigned char* sig, size_t size, size_t start_at = 0) {
-		if (size <= 64) {
-			static const unsigned int chunkSize = 16000;
-			if (!file) {
-				file.close();
-				file.open(path, std::ios::in | std::ios::binary);
+
+	size_t FindSignature(std::vector<BYTE> sig, size_t start_at = 0) {
+		for (size_t offset = start_at; offset < fileSize - sig.size(); offset++) {
+			if (offset >= 0x600) {
+				int stophere = 0;
 			}
-			for (size_t i = start_at; i < fileSize; i += chunkSize) {
-				file.seekg(i);
-				unsigned char bytes[chunkSize];
-				memset(bytes, 0, chunkSize);
-				file.read((char*)&bytes, chunkSize);
-				for (size_t k = 0; k < chunkSize; k++) {
-					if (bytes[k] == sig[0]) {
-						auto offset = i + k;
-						file.seekg(offset);
-						unsigned char bytesArray[64];
-						memset(bytesArray, 0, 64);
-						file.read((char*)&bytesArray, size);
-						bool equals = CompareByteArray(bytesArray, sig, size);
-						if (equals) return offset;
-					}
+			if (fileBytes[offset] == sig[0]) {
+				bool equals = CompareByteArray(reinterpret_cast<BYTE*>(fileBytes + offset + 1), sig.data() + 1, sig.size() - 1);
+				if (equals) {
+					return offset;
 				}
 			}
 		}
 		return 0;
 	}
-	uint32_t FindOffset(unsigned char* sig, size_t size, size_t start_at = 0) {
-		auto instructionAddr = FindSignature(sig, size, start_at);
-		if (instructionAddr == 0) return 0;
-		file.seekg(instructionAddr + 3);
-		char lEndian[4];
-		file.read(lEndian, 4);
-		auto rOffset = GetInt(lEndian);
-		return instructionAddr + 7 + rOffset + codeOffset;
+	uint32_t FindOffset(std::vector<BYTE>* sigv, uint32_t size, size_t start_at = 0) {
+		for (auto i = 0; i < size; i++) {
+			auto sig = sigv[i];
+			auto offset = FindSignature(sig, start_at);
+			if (!offset) continue;
+			auto k = 0;
+			for (; sig[k]; k++);
+			uint32_t nextOffset = *reinterpret_cast<uint32_t*>(fileBytes + offset + k);
+			return offset + k + 4 + nextOffset + codeOffset;
+		}
+		return 0;
 	}
 public:
-	size_t LoadFile(const char* _path) {
-		fileSize = 0;
-		file = std::ifstream(_path, std::ios::binary);
-		if (file.is_open()) {
-			path = _path;
-
-			std::vector<unsigned char> bytes(sizeof(IMAGE_DOS_HEADER));
-
-			file.read((char*)bytes.data(), sizeof(IMAGE_DOS_HEADER));
-			auto new_header_ptr = reinterpret_cast<PIMAGE_DOS_HEADER>(bytes.data())->e_lfanew;
-
-			file.seekg(new_header_ptr);
-			bytes.reserve(sizeof(IMAGE_NT_HEADERS));
-			file.read((char*)bytes.data(), sizeof(IMAGE_NT_HEADERS));
-			auto baseOfCode = reinterpret_cast<PIMAGE_NT_HEADERS>(bytes.data())->OptionalHeader.BaseOfCode;
-
-			file.read((char*)bytes.data(), sizeof(IMAGE_SECTION_HEADER));
-			auto rawDataPtr = reinterpret_cast<PIMAGE_SECTION_HEADER>(bytes.data())->PointerToRawData;
-
+	size_t LoadFile(const char* path) {
+		auto file = CreateFileA(path, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+		if (file != INVALID_HANDLE_VALUE) {
+			LARGE_INTEGER size;
+			GetFileSizeEx(file, &size);
+			fileSize = size.QuadPart;
+			fileBytes = new BYTE[fileSize];
+			DWORD readed;
+			if (!ReadFile(file, fileBytes, fileSize, &readed, 0) || readed != fileSize) {
+				return 0;
+			};
+			auto new_header_ptr = reinterpret_cast<PIMAGE_DOS_HEADER>(fileBytes)->e_lfanew;
+			auto baseOfCode = reinterpret_cast<PIMAGE_NT_HEADERS>(fileBytes + new_header_ptr)->OptionalHeader.BaseOfCode;
+			auto rawDataPtr = reinterpret_cast<PIMAGE_SECTION_HEADER>(fileBytes + new_header_ptr + sizeof(IMAGE_NT_HEADERS))->PointerToRawData;
 			codeOffset = baseOfCode - rawDataPtr;
-
-			file.seekg(0, file.end);
-			fileSize = file.tellg();
+			CloseHandle(file);
 		}
 		return fileSize;
 	};
 	uint32_t GetGWorld() {
-		static unsigned char sig[] = { 0x48, 0x8B, 0x1D, 0x00, 0x00, 0x00, 0x00, 0x48, 0x85, 0xDB, 0x74, 0x00, 0x41, 0xB0, 0x01 };
-		world_offset = FindOffset(sig, sizeof(sig));
+		if (!world_offset) {
+			static std::vector<BYTE> sigv[] = { { 0x48, 0x8B, 0x1D, 0x00, 0x00, 0x00, 0x00, 0x48, 0x85, 0xDB, 0x74, 0x00, 0x41, 0xB0, 0x01 }, { 0x48, 0x8b, 0x05, 0x00, 0x00, 0x00, 0x00, 0x75, 0x1b }, {0x48, 0x8B, 0x05, 0x00, 0x00, 0x00, 0x00, 0x48, 0x8B, 0x88, 0x00, 0x00, 0x00, 0x00, 0x48, 0x85, 0xC9, 0x74, 0x06, 0x48, 0x8B, 0x49, 0x70} };
+			world_offset = FindOffset(sigv, 3);
+		}
 		return world_offset;
 	}
-	size_t GetGObjects() {
-		static unsigned char sig[] = { 0x48, 0x8B, 0x05, 0x00, 0x00, 0x00, 0x00, 0x48, 0x8B, 0x0C, 0xC8, 0x48, 0x8D, 0x04, 0xD1, 0xEB };
-		objects_offset = FindOffset(sig, sizeof(sig));
+	uint32_t GetGObjects() {
+		if (!objects_offset) {
+			static std::vector<BYTE> sigv[] = { {0x48, 0x8B, 0x05, 0x00, 0x00, 0x00, 0x00, 0x48, 0x8B, 0x0C, 0xC8, 0x48, 0x8D, 0x04, 0xD1, 0xEB}, {0x48 , 0x8b , 0x0d , 0x00 , 0x00 , 0x00 , 0x00 , 0x81 , 0x4c , 0xd1 , 0x08 , 0x00 , 0x00 , 0x00 , 0x40} };
+			objects_offset = FindOffset(sigv, 2);
+		}
 		return objects_offset;
 	}
-	uint64_t GetGNames() {
-		static unsigned char sigv[][12] = { { 0x48, 0x8B, 0x05, 0x00, 0x00, 0x00, 0x00, 0x48, 0x85, 0xC0, 0x75, 0x5F }, { 0x48, 0x8D, 0x15, 0x00, 0x00, 0x00, 0x00, 0xEB, 0x00, 0x48, 0x8D, 0x0D } };
-		for (unsigned char i = 0; i < sizeof(sigv)/sizeof(sigv[0]); i++) {
-			names_offset = FindOffset(sigv[i], sizeof(sigv[i]));
-			if (names_offset) break;
+	uint32_t GetGNames() {
+		if (!names_offset) {
+			static std::vector<BYTE> sigv[] = { { 0x48, 0x8B, 0x05, 0x00, 0x00, 0x00, 0x00, 0x48, 0x85, 0xC0, 0x75, 0x5F }, { 0x48, 0x8D, 0x15, 0x00, 0x00, 0x00, 0x00, 0xEB, 0x00, 0x48, 0x8D, 0x0D }, {0x48, 0x8b, 0x3d, 0x00, 0x00, 0x00, 0x00, 0x48, 0x85, 0xff, 0x75, 0x3c} };
+			names_offset = FindOffset(sigv, 3);
 		}
 		return names_offset;
+	}
+
+	uint32_t GetNamePool() {
+		if (!pool_offset) {
+			static std::vector<BYTE> sigv[] = { { 0x48, 0x8d, 0x35, 0x00, 0x00, 0x00, 0x00, 0xeb, 0x16 } };
+			pool_offset = FindOffset(sigv, 1);
+		}
+		return pool_offset;
 	}
 };
 
 int main(int argc, const char* argv[]) {
 	if (argc != 2) {
-		std::cout << "Usage:\t.\\updater.exe \"pathToUE4Game\"\n";
+		printf("Usage: .\\%ls \"pathToUE4Game\"\n", std::filesystem::path(argv[0]).filename().c_str());
 		return 0;
 	}
 	Updater updater;
 	if (!updater.LoadFile(argv[1])) return 0;
-	std::cout << "World\t:\t" << std::hex << updater.GetGWorld() << std::endl;
+	printf("%-17s|\t%.7X\n", "UWorld", updater.GetGWorld());
 	Beep(370, 100);
-	std::cout << "Objects\t:\t" << std::hex << updater.GetGObjects() << std::endl;
+	printf("%-17s|\t%.7X\n", "TUObjectArray", updater.GetGObjects());
 	Beep(370, 100);
-	std::cout << "Names\t:\t" << std::hex << updater.GetGNames() << std::endl;
+	printf("%-17s|\t%.7X\n", "TNameEntryArray", updater.GetGNames());
 	Beep(370, 100);
+	printf("%-17s|\t%.7X\n", "FNamePool", updater.GetNamePool());
+	Beep(370, 100);
+
+
 	return 1;
 }
